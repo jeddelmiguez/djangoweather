91b7a6110fdfee85946a19c0874f270a1cba8c79
Jed Delmiguez <jdelmiguez@gmail.com>
2020-03-19 06:04:52 +1000

    added jumbotron bootstrap to api code

--- lookup/views.py
+++ lookup/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+
+    api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=25&API_KEY=C6F3C94C-EEB0-4D9A-9D9A-EBA51FD8EDFB")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
